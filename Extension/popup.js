3e12b12fb8e1e9cfc1e16b47693856a1e8b38761
Mini99 <manvir.dhillon30@gmail.com>
2020-10-24 21:22:49 -0700

    testing yt

--- Extension/popup.js
+++ Extension/popup.js
@@ -10,8 +10,9 @@ document.addEventListener('DOMContentLoaded', function () {
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         // document.getElementById('Current_url').value = tabs[0].url;
         let tabUrl = tabs[0].url
-        var ytID = str.split("https://www.youtube.com/watch?v=");
-        console.log(ytID);
+        //var ytID = str.split("https://www.youtube.com/watch?v=");
+        //console.log(ytID);
+        console.log(tabUrl);
       });
     }
   });
