78a0cac16f3810d7d4a8b5e89e73970e9bef4d70
Mini99 <manvir.dhillon30@gmail.com>
2020-10-24 21:20:54 -0700

    parsing ytID

--- Extension/popup.js
+++ Extension/popup.js
@@ -10,7 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         // document.getElementById('Current_url').value = tabs[0].url;
         let tabUrl = tabs[0].url
-        console.log(tabUrl);
+        var ytID = str.split("https://www.youtube.com/watch?v=");
+        console.log(ytID);
       });
     }
   });
