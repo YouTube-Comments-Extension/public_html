9f7fb0bd6fa92641af2b8f821661bc1e0cd0165f
zoroark45 <guerreroelsa30@gmail.com>
2020-10-26 00:10:51 -0700

    Centered Video on the video.php page with div tags

--- php/video.php
+++ php/video.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET['ytid'])) {
-  echo "<iframe width='1280' height='720' src='https://www.youtube.com/embed/".$_GET['ytid']."' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+  echo "<div align='center'><iframe width='100%' height='720' src='https://www.youtube.com/embed/".$_GET['ytid']."' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div>";
 }
 
 ?>
