2e97eb8938f10231957ce43a12ce69664d71ec3e
zoroark45 <guerreroelsa30@gmail.com>
2020-10-26 11:52:50 -0700

    created a ytid variable in video.php

--- php/video.php
+++ php/video.php
@@ -1,4 +1,7 @@
 <?php
+
+$ytid = "";
+
 if (isset($_GET['ytid'])) {
   echo "<div align='center'><iframe width='1280' height='720' src='https://www.youtube.com/embed/".$_GET['ytid']."' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div>";
 }
