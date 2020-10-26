fc101c9f39767f6f10eb64920218aec01fa7c8c4
zoroark45 <guerreroelsa30@gmail.com>
2020-10-26 11:54:12 -0700

    created a error message for empty ytid in url

--- php/video.php
+++ php/video.php
@@ -4,6 +4,8 @@ $ytid = "";
 
 if (isset($_GET['ytid'])) {
   echo "<div align='center'><iframe width='1280' height='720' src='https://www.youtube.com/embed/".$_GET['ytid']."' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div>";
+}else{
+  echo "This page does not exist";
 }
 
 ?>
