c9ee5e01b9b410f7f3ff529b0a89df5fa8b78971
zoroark45 <guerreroelsa30@gmail.com>
2020-10-27 10:31:28 -0700

    added a query for ytid from ytvideo database in video.php

--- php/video.php
+++ php/video.php
@@ -1,11 +1,17 @@
 <?php
 
+include(./classes/DB.php);
+
 $ytid = "";
 
+
 if (isset($_GET['ytid'])) {
+  $ytid = $_GET['ytid'];
+  if(DB::query('SELECT ytid FROM ytvideo WHERE ytid=:ytid', array(':ytid'=>$ytid))){
   echo "<div align='center'><iframe width='1280' height='720' src='https://www.youtube.com/embed/".$_GET['ytid']."' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div>";
-}else{
-  echo "This page does not exist";
+} else{
+  echo "video not found";
+}
 }
 
 ?>
