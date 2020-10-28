ebd9c8340bba9bc73c4b4b8e6190f38426d5667e
zoroark45 <guerreroelsa30@gmail.com>
2020-10-27 21:39:52 -0700

    added who the video is by in the list.php when displaying the title as well

--- php/list.php
+++ php/list.php
@@ -20,10 +20,11 @@ foreach($ytid as $video){ // get all the videos in the database, display their t
     $ytsnippetURL = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id=' . $video[0] . '&key=' . $apiPublicKey;
     $snippetResponse = file_get_contents($ytsnippetURL);
     $snippetJson = json_decode($snippetResponse);
-    $videoTitle = $snippetJson->items[0]->snippet->title;    
+    $videoTitle = $snippetJson->items[0]->snippet->title;
+    $videoChannel = $snippetJson->items[0]->snippet->channelId;   
     $backgroundColor = selectColor::random_color();
     echo '<div style="width:500px;display:inline-block;background-color:#'.$backgroundColor.';">';
-    echo '<h4 style="display:table;margin-top: 0;margin-bottom: 0;position:relative;text-align:center;background-color:rgba(255, 255, 255, 0.5);">'.$videoTitle.'</h4>';
+    echo '<h4 style="display:table;margin-top: 0;margin-bottom: 0;position:relative;text-align:center;background-color:rgba(255, 255, 255, 0.5);">'.$videoTitle.' by '.$videoChannel.'</h4>';
     echo '<a href="https://www.replytu.be/video.php?ytid='.$video[0].'"><img src="https://i3.ytimg.com/vi/'.$video[0].'/maxresdefault.jpg" height="240" width="426"></a>'."<br>"."<br>";
     echo '</div>';
     
