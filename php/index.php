ff19331a5905fbc23860b91b898bcba459339b02
zoroark45 <guerreroelsa30@gmail.com>
2020-10-27 23:27:01 -0700

    changed echos to print html in php pages index, addvideo, list,video

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
     echo '<body style="margin-top: 0;margin-bottom: 0;background-color:#181818;">';
-    echo '<h1 style="margin-top: 0;margin-bottom: 0;position:relative;color:white;font-family: Roboto, Arial, sans-serif;">Welcome!</h1>';
+    echo '<h1 style="margin-top: 0;margin-bottom: 0;position:relative;color:white;font-family: Roboto, Arial, sans-serif;" align="center">Welcome!</h1>';
     echo '</body>';
 ?>
