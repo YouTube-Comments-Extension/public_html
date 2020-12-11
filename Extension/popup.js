bdfea7734594f81574b29b69a26ed4bbd9c36416
Manvir Dhillon <manvir.dhillon30@gmail.com>
2020-12-10 20:17:52 -0800

    made ui and js changes

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,21 +1,59 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var link = document.getElementById('btn');
-    // onClick's logic below:
-    link.addEventListener('click', function () {
-      AddUrl();
-    });
-  
-  
-    function AddUrl() {
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        var str = tabs[0].url
-        if(str.startsWith("https://www.youtube.com/watch?v=")){
-          var res = str.slice(32,43);
-          window.open("https://replytu.be/", '_blank');
-        }
-        else{
-          document.getElementById("demo").innerHTML = "Invalid URL";
-        }
-      });
+  var link = document.getElementById('btn');
+
+  //Display Button Message
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var str = tabs[0].url
+    if(str === "https://replytu.be/"){ //On ReplyTube homepage
+      document.getElementById("btn").innerHTML = "Thanks for downloading!";
+      return;
+    }
+    if(str.startsWith("https://replytu.be/video.php") || str.startsWith("https://replytu.be/video?ytid=")){ //On ReplyTube Video
+        document.getElementById("btn").innerHTML = "Take me back to YouTube";
+    }
+    else{ //On YouTube Video
+        document.getElementById("btn").innerHTML = "Take me to ReplyTube!";
     }
   });
+
+  //OnClick Event Function
+  link.addEventListener('click', function () {
+    AddUrl();
+  });
+  
+  //Chrome Cookies
+  chrome.cookies.set({"name":"AddVideo","url":"https://replytu.be/" ,"value":"Dummy Data", expirationDate: (new Date().getTime()/1000) + 30},function (cookie){
+    console.log(JSON.stringify(cookie));
+    console.log(chrome.extension.lastError);
+    console.log(chrome.runtime.lastError);
+});
+
+  //OnClick Event Actions (Button Click)
+  function AddUrl() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      var str = tabs[0].url
+      if(str === "https://replytu.be/"){ //On ReplyTube homepage
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank'); //Rick Roll
+      }
+      else if(str.startsWith("https://replytu.be/video.php?ytid=")){ //On ReplyTube Video
+        var res = str.slice(34,45);
+        var newUrl = "https://www.youtube.com/watch?v=" + res;
+        window.open(newUrl, '_blank');
+      }
+      else if(str.startsWith("https://replytu.be/video?ytid=")){ //On ReplyTube Video List
+        var res = str.slice(30,41);
+        var newUrl = "https://www.youtube.com/watch?v=" + res;
+        window.close();
+        window.open(newUrl, '_blank');
+      }
+      else if(str.startsWith("https://www.youtube.com/watch?v=")){ //On YouTube Video
+        var res = str.slice(32,43);
+        var newUrl = "https://replytu.be/video.php?ytid=" + res;
+        window.open(newUrl, '_blank');
+      }
+      else{
+        document.getElementById("demo").innerHTML = "Invalid URL"; //On any other URL
+      }
+    });
+  }
+});
